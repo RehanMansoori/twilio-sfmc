5806e9afe3d90f01cd1fc4ab16d30b8e7b770f1a
RehanMansoori <115612082+RehanMansoori@users.noreply.github.com>
2022-11-09 15:52:20 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -66,8 +66,6 @@ exports.save = function (req, res) {
     console.log("5 -- For Save");	
     console.log("4");	
     console.log("3");	
-    console.log("2");	
-    console.log("1");	
     //console.log("Saved: "+req.body.inArguments[0]);
     
     // Data from the req and put it in an array accessible to the main app.
@@ -84,9 +82,27 @@ exports.execute = function (req, res) {
     console.log("5 -- For Execute");	
     console.log("4");	
     console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Executed: "+req.body.inArguments[0]);
+    var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://login.salesforce.com/services/oauth2/token',
+  'headers': {
+    'Content-Type': 'application/json',
+    'Cookie': 'BrowserId=37NM5lnREe2Ik1X6ObvKKA; CookieConsentPolicy=0:0; LSKey-c$CookieConsentPolicy=0:0'
+  },
+  formData: {
+    'client_id': '3MVG9n_HvETGhr3BTx_IIe00PIjo01Q79Srg0EYI1npGcSgwizYCv9xQgaDdIX2eo593elKpWzc6T4DwCJCQ3',
+    'client_secret': 'FA19B10545EDFBC7BD2EFCF2BDDA570D732E14E03B9CF9026202D8CAE17C19F3',
+    'grant_type': 'password ',
+    'username': 'rehan@mansoori.com',
+    'password': '785392more'
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
+
     
     logData(req);
     res.send(200, 'execute');
